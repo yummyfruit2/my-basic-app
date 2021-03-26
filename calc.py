65c7a72cf9a1350926524adb46beb97e4f8cb074
yummyfruit2 <rishkamath@hotmail.com>
2021-03-26 15:51:59 +0530

    Change error messages

--- calc.py
+++ calc.py
@@ -7,7 +7,7 @@ while calculate:
         x = int(input("Enter your 1st number :"))
         y = int(input("Enter your 2nd number :"))
     except(ValueError):
-        print("not an integer")
+        print("It is not an integer")
     
     else:
         o = input("enter your operator in words :")
